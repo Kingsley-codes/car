293bb1cd397b5d24a8aad5edf435ca4990afd0a1
Kingsley-codes <agbamkingsley@gmail.com>
2025-02-07 12:50:23 +0100

    one

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -4,7 +4,7 @@ import TeddyBearHug from "./Teddy";
 const Popup = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="p-6 rounded-lg shadow-lg w-2/4 h-3/4 bg-pink-100 relative overflow-hidden">
+      <div className="p-6 rounded-lg shadow-lg w-2/4 bg-pink-100 relative overflow-hidden">
         
         <h2 className="text-5xl my-2 text-[rgb(255,0,0)] font-bold text-center font-dancing">Yay!!</h2>
         <p className="font-great text-center mt-3 mb-5 text-3xl text-[rgb(255,0,0)]">Valentine don set!</p>
